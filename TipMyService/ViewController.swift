d3a78d70d306c03aabe497bf7b7000a0df3fc7e8
Treybrowder <tbrowder7@gmail.com>
2024-05-08 14:49:11 -0400

    initial setup

---
+++ TipMyService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipMyService
+//
+//  Created by Trey Browder on 5/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
